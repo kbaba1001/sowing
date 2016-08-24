6c90d9cb4eeecf5441c652c7d4a8b949a06d974b
kbaba1001 <kbaba1001@users.noreply.github.com>
2016-08-24 23:07:25 +0900

    adjust test

--- test/lib/test_runner.rb
+++ test/lib/test_runner.rb
@@ -1,30 +1,26 @@
 class TestRunner < Sowing::TestBase
-  sub_test_case '#create' do
-    test 'insert users from csv' do
-      runner = Sowing::Runner.new(data_directory: 'test/fixtures/csv')
-
-      runner.create(User)
-
-      assert { match_array?(User.pluck(:first_name), %w(Carlotta 中平)) }
-      assert { match_array?(User.pluck(:last_name), %w(Wilkinson 薫)) }
-    end
+  setup do
+    @runner = Sowing::Runner.new(data_directory: 'test/fixtures/csv')
   end
 
-  sub_test_case '#create_or_do_nothing' do
-    test 'insert users form csv' do
-      runner = Sowing::Runner.new(data_directory: 'test/fixtures/csv')
+  test 'insert users from csv' do
+    @runner.create(User)
 
-      runner.create_or_do_nothing(User, :first_name)
+    assert { match_array?(User.pluck(:first_name), %w(Carlotta 中平)) }
+    assert { match_array?(User.pluck(:last_name), %w(Wilkinson 薫)) }
+  end
 
-      assert { match_array?(User.pluck(:first_name), %w(Carlotta 中平)) }
-      assert { match_array?(User.pluck(:last_name), %w(Wilkinson 薫)) }
+  test 'insert users or do nothing form csv' do
+    @runner.create_or_do_nothing(User, :first_name)
 
-      runner.create_or_do_nothing(User, :first_name)
+    assert { match_array?(User.pluck(:first_name), %w(Carlotta 中平)) }
+    assert { match_array?(User.pluck(:last_name), %w(Wilkinson 薫)) }
 
-      assert { User.where(first_name: 'Carlotta').count == 1 }
+    @runner.create_or_do_nothing(User, :first_name)
 
-      user = User.find_by(first_name: 'Carlotta')
-      assert { user.created_at == user.updated_at }
-    end
+    assert { User.where(first_name: 'Carlotta').count == 1 }
+
+    user = User.find_by(first_name: 'Carlotta')
+    assert { user.created_at == user.updated_at }
   end
 end
